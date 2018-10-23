c1fc93147aa94e4c6ba4c9eecacc7a906ef068e0
PesTospertnyj <43485813+PesTospertnyj@users.noreply.github.com>
2018-10-23 18:47:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Flower rose = new Rose();
+        Flower tulip = new Tulip();
+        System.out.println();
+    }
+}
